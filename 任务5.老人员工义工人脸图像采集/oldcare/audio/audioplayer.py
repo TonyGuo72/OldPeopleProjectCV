c79f4a9d974c4b10b96c7cd305b19c9ac5359d77
kambition <1241110779@qq.com>
2022-07-14 09:21:27 +0800

    beishi
    first change

---
+++ 任务5.老人员工义工人脸图像采集/oldcare/audio/audioplayer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+audio player
+'''
+
+# import library
+from subprocess import call
+
+# play audio
+from pygame import mixer
+
+from pygame import mixer
+
+
+def play_audio(audio_name):
+    try:
+        mixer.init()
+        mixer.music.load(audio_name)
+        mixer.music.play()
+    except KeyboardInterrupt as e:
+        print(e)
+    finally:
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+    
